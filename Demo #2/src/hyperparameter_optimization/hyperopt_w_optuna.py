a00893f7a86b75a18d15d458cb635479b70a5961
Bence Bial <gbenceg@gmail.com>
2022-05-23 22:23:25 +0200

    Hyperopt w Optuna

---
+++ Demo #2/src/hyperparameter_optimization/hyperopt_w_optuna.py
@@ -0,0 +1,40 @@
+import optuna
+from data.data import import_data
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from multiprocessing import cpu_count
+from sklearn.pipeline import Pipeline
+from modeling.models import (
+    get_imputed_preprocessor,
+    train_eval
+)
+
+
+def objective(trial):
+    X, y = import_data(file='train')
+    
+    # Fix parameters
+    params = {
+        "random_state": 0,
+        "n_jobs": cpu_count(),
+        "gamma": 0,
+        "subsample": 1.0
+    }
+
+    # Hyperparameters to optimize
+    params["max_depth"] = trial.suggest_int("max_depth", 3, 10)
+    params["learning_rate"] =  trial.suggest_float('learning_rate', 1e-2, 1, log=True)
+    params["n_estimators"] = trial.suggest_int("n_estimators", 50, 1000)
+
+    model, valid_perf = train_eval(
+        X=X,
+        y=y,
+        model=Pipeline(steps=[
+            ("preprocessor", get_imputed_preprocessor()),
+            ("regressor", XGBRegressor(**params))
+        ]),
+        model_params=params,
+        model_name="xgb_imputed",
+    )
+
+    return valid_perf["RMSE"]
