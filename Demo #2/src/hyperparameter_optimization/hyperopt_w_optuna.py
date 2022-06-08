180f299885f92c3231aa5e07933c74b10e724dfa
Bence Bial <gbenceg@gmail.com>
2022-06-09 00:14:58 +0200

    rerun hyperopt

--- Demo #2/src/hyperparameter_optimization/hyperopt_w_optuna.py
+++ Demo #2/src/hyperparameter_optimization/hyperopt_w_optuna.py
@@ -1,12 +1,11 @@
 from multiprocessing import cpu_count
 
-import optuna
-from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from xgboost import XGBRegressor
 
 from data.data import import_data
-from modelling.models import get_imputed_preprocessor, train_eval
+from modelling.models import train_eval
+from modelling.preprocessing import get_constant_imputed_preprocessor
 
 
 def objective(trial):
@@ -20,13 +19,12 @@ def objective(trial):
     params["learning_rate"] = trial.suggest_float("learning_rate", 1e-2, 1, log=True)
     params["n_estimators"] = trial.suggest_int("n_estimators", 50, 1000)
 
-    # TODO: evaluate on validation set, not on test set
-    model, test_perf = train_eval(
+    model, training_perf, test_perf = train_eval(
         X=X,
         y=y,
         model=Pipeline(
             steps=[
-                ("preprocessor", get_imputed_preprocessor()),
+                ("preprocessor", get_constant_imputed_preprocessor()),
                 ("regressor", XGBRegressor(**params)),
             ]
         ),
